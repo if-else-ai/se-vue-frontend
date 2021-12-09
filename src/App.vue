dc9d7874511dda91e74a84e98e631323fccee96f
Zebra <Thirawatp9@gmail.com>
2021-12-09 22:40:29 +0700

    Product card

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,13 @@
 <template>
-	<v-app>
-		<Navigation
-			v-if="currentRouteName !== 'login'"
-			:navTitle="currentRouteName"
-		/>
-		<v-main>
-			<router-view></router-view>
-		</v-main>
-	</v-app>
+  <v-app>
+    <Navigation
+      v-if="currentRouteName !== 'login'"
+      :navTitle="currentRouteName"
+    />
+    <v-main>
+      <router-view></router-view>
+    </v-main>
+  </v-app>
 </template>
 
 <script>
@@ -15,31 +15,29 @@ import Navigation from "@/components/navigation/navigation.vue";
 // views handle app interface / ui
 // can handle some form validation work before sending data to back-end
 export default {
-	name: "App",
+  name: "App",
 
-	components: {
-		Navigation,
-	},
+  components: {
+    Navigation,
+  },
 
-	data: () => ({}),
+  data: () => ({}),
 
-	created() {
-		this.$store.dispatch("getProducts");
-	},
+  created() {
+    this.$store.dispatch("getProducts");
+  },
 
-	computed: {
-		// display nav route name
-		currentRouteName() {
-			
-			return this.$route.name;
-		},
-	},
+  computed: {
+    // display nav route name
+    currentRouteName() {
+      return this.$route.name;
+    },
+  },
 };
 </script>
 
 <style>
 * {
-	font-family: "Prompt", sans-serif;
+  font-family: "Prompt", sans-serif;
 }
-
 </style>
---
+++ src/components/Product.vue
@@ -0,0 +1,50 @@
+<template>
+  <v-card class="product-card" max-width="374">
+    <v-img height="250px" :src="product.src" class="image" />
+    <v-card-title>{{ product.title }}</v-card-title>
+
+    <v-card-text class="description">
+      <p>{{ product.detail }}</p>
+    </v-card-text>
+
+    <v-divider></v-divider>
+
+    <v-card>
+      <v-row>
+        <v-col cols="auto" class="mr-auto price">
+          <p>฿{{ product.price }}</p>
+        </v-col>
+        <v-col cols="auto" class="selled">
+          <p>ขายแล้ว {{ product.selled }} ชิ้น</p>
+        </v-col>
+      </v-row>
+    </v-card>
+  </v-card>
+</template>
+
+<script>
+export default {
+  name: "product",
+  props: ["product"],
+};
+</script>
+
+<style>
+.product-card {
+  margin: 20px;
+}
+.description p {
+  color: rgb(36, 36, 36);
+  height: 60px;
+}
+.price p {
+  color: rgb(0, 162, 255);
+  padding-top: 10px;
+  padding-left: 15px;
+}
+.selled p {
+  color: rgb(206, 206, 206);
+  padding-top: 10px;
+  padding-right: 15px;
+}
+</style>
