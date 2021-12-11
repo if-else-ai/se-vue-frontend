aa1434786d98399b49a755fd3308b1c6c848486f
mcca123 <mccab9@gmail.com>
2021-12-12 02:48:35 +0700

    register
    เพิ่มหน้าลงทะเบียนเปล่าๆ

--- src/router/index.js
+++ src/router/index.js
@@ -7,66 +7,71 @@ Vue.use(VueRouter);
 
 // define app route
 const routes = [
-	{
-		path: "/",
-		redirect: "/home",
-	},
-	{
-		path: "/shopping",
-		name: "shopping",
-		component: () => import("../views/shopping.vue"),
-	},
-	{
-		path: "/shopping/product",
-		name: "product",
-		component: () => import("../views/product.vue"),
-	},
-	{
-		path: "/home",
-		name: "home",
-		component: () => import("../views/home.vue"),
-	},
-	// {
-	// 	path: "/keyboard",
-	// 	name: "keyboard",
-	// 	component: () => import("../views/keyboard.vue"),
-	// },
-	{
-		path: "/customize",
-		name: "customize",
-		component: () => import("../views/customize.vue"),
-	},
-	// {
-	// 	path: "/accesories",
-	// 	name: "accesories",
-	// 	component: () => import("../views/accesories.vue"),
-	// },
-	// {
-	// 	path: "/tools",
-	// 	name: "tools",
-	// 	component: () => import("../views/tools.vue"),
-	// },
-	{
-		path: "/login",
-		name: "login",
-		component: () => import("../views/login.vue"),
-	},
-	{
-		path: "/cart",
-		name: "cart",
-		component: () => import("../views/cart.vue"),
-	},
-	{
-		path: "/profile",
-		name: "profile",
-		component: () => import("../views/profile.vue"),
-	},
+  {
+    path: "/",
+    redirect: "/home",
+  },
+  {
+    path: "/shopping",
+    name: "shopping",
+    component: () => import("../views/shopping.vue"),
+  },
+  {
+    path: "/shopping/product",
+    name: "product",
+    component: () => import("../views/product.vue"),
+  },
+  {
+    path: "/home",
+    name: "home",
+    component: () => import("../views/home.vue"),
+  },
+  // {
+  // 	path: "/keyboard",
+  // 	name: "keyboard",
+  // 	component: () => import("../views/keyboard.vue"),
+  // },
+  {
+    path: "/customize",
+    name: "customize",
+    component: () => import("../views/customize.vue"),
+  },
+  // {
+  // 	path: "/accesories",
+  // 	name: "accesories",
+  // 	component: () => import("../views/accesories.vue"),
+  // },
+  // {
+  // 	path: "/tools",
+  // 	name: "tools",
+  // 	component: () => import("../views/tools.vue"),
+  // },
+  {
+    path: "/login",
+    name: "login",
+    component: () => import("../views/login.vue"),
+  },
+  {
+    path: "/cart",
+    name: "cart",
+    component: () => import("../views/cart.vue"),
+  },
+  {
+    path: "/profile",
+    name: "profile",
+    component: () => import("../views/profile.vue"),
+  },
+  {
+    path: "/register",
+    name: "register",
+    component: () => import("../views/register.vue"),
+  },
 ];
 
 const router = new VueRouter({
-	// using 'stack'
-	mode: "history",
-	routes,
+  // using 'stack'
+  mode: "history",
+  routes,
 });
 
 export default router;
---
+++ src/views/register.vue
@@ -0,0 +1,14 @@
+<template>
+  <v-container>
+
+  </v-container>
+</template>
+
+<style src="./style.css"></style>
+
+<script>
+
+export default {
+  data: () => ({}),
+};
+</script>
