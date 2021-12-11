26ef9e44e7d5216b8a32f4ba79fe421f06967fae
mcca123 <mccab9@gmail.com>
2021-12-12 03:10:22 +0700

    register
    register ที่มาพร้อมกับ login น่าจะเหลือแค่เชื่อม API

--- src/views/register.vue
+++ src/views/register.vue
@@ -76,6 +76,52 @@
 <script>
 
 export default {
-  data: () => ({}),
+  data: () => ({
+    computed: {
+        passwordMatch() {
+            return () => this.password === this.verify || "Password must match";
+        }
+    },
+    methods: {
+        validate() {
+            if (this.$refs.loginForm.validate()) {
+        // submit form to server/API here...
+        }
+        },
+        reset() {
+            this.$refs.form.reset();
+        },
+        resetValidation() {
+            this.$refs.form.resetValidation();
+        }
+    },
+    dialog: true,
+    tab: 0,
+    tabs: [
+        {name:"Login", icon:"mdi-account"},
+        {name:"Register", icon:"mdi-account-outline"}
+    ],
+    valid: true,
+    firstName: "",
+    lastName: "",
+    email: "",
+    password: "",
+    verify: "",
+    loginPassword: "",
+    loginEmail: "",
+    loginEmailRules: [
+        v => !!v || "Required",
+        v => /.+@.+\..+/.test(v) || "E-mail must be valid"
+    ],
+    emailRules: [
+      v => !!v || "Required",
+      v => /.+@.+\..+/.test(v) || "E-mail must be valid"
+    ],
+    show1: false,
+    rules: {
+        required: value => !!value || "Required.",
+        min: v => (v && v.length >= 8) || "Min 8 characters"
+    }
+  }),
 };
 </script>
